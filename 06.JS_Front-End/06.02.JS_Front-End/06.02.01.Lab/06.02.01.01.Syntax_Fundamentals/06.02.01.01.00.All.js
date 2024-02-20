31bcb80d723f0cee52b8877d4e949f16c759c888
konstakostov <122868401+konstakostov@users.noreply.github.com>
2024-02-20 21:17:16 +0200

    JS Front-End, Lab 01

---
+++ 06.JS_Front-End/06.02.JS_Front-End/06.02.01.Lab/06.02.01.01.Syntax_Fundamentals/06.02.01.01.00.All.js
@@ -0,0 +1,39 @@
+// // 01. Multiply NUmber by 2
+
+
+
+// // 02. Student Information
+
+
+
+// // 03. Excellent Grade
+
+
+
+// // 04. Month Printer
+
+
+
+// // 05. Math Operations
+
+
+
+// // 06. Largest Number
+
+
+
+// // 07. Theatre Promotions
+
+
+
+// // 08. Circle Area
+
+
+
+// // 09. Numbers from 1 to 5
+
+
+
+// // 10. Numbers from M to N
+
+
