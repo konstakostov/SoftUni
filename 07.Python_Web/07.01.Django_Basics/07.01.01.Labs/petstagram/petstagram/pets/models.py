4b6c04843e79ef1ecf4b6dd01577c7229e89d9ae
K. Kostov <122868401+konstakostov@users.noreply.github.com>
2024-09-29 21:15:43 +0300

    Update petstagram

--- 07.Python_Web/07.01.Django_Basics/07.01.01.Labs/petstagram/petstagram/pets/models.py
+++ 07.Python_Web/07.01.Django_Basics/07.01.01.Labs/petstagram/petstagram/pets/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30,
+    )
+
+    personal_pet_photo = models.URLField()
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    slug = models.SlugField(
+        null=False,
+        blank=True,
+        unique=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        super().save(*args, **kwargs)
--- 07.Python_Web/07.01.Django_Basics/07.01.01.Labs/petstagram/petstagram/settings.py
+++ 07.Python_Web/07.01.Django_Basics/07.01.01.Labs/petstagram/petstagram/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram',
+        'USER': 'charlie',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
